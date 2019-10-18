053d137fc69a20650f9eef1d8b526ae34774a002
deltagphys <cty.jgates@gmail.com>
2019-10-18 16:35:22 -0400

    method completed, test added

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -1,8 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static org.junit.Assert.*;
 
+@RunWith(BlockJUnit4ClassRunner.class)
 public class LogginLabTest {
     private final static Logger logger = Logger.getLogger(LogginLab.class.getName());
 
@@ -28,6 +32,24 @@ public class LogginLabTest {
             } else {
                 logger.log(Level.INFO, "Threshold finally reached!");
                 assertFalse(lab.thresholdExceeds(i));
+            }
+        }
+    }
+
+    @org.junit.Test
+    public void thresholdReached() {
+        Integer finalLimit = 5;
+
+        LogginLab lab = new LogginLab();
+        lab.setThreshold(finalLimit);
+
+        for (Integer i = 1; i <= 2*finalLimit; i++) {
+            if (i <= finalLimit) {
+                logger.log(Level.INFO, "Threshold not reached! It is "+i);
+                assertFalse(lab.thresholdReached(i));
+            } else {
+                logger.log(Level.INFO, "Threshold finally reached!");
+                assertTrue(lab.thresholdReached(i));
             }
         }
     }
